8da66d64a6b2e77825e0dfd0d1d9055813862d51
rashidshafeev <rashidshafeev@gmail.com>
2021-06-05 17:49:51 +0300

    Урок 3
    Попробовал несколько способов, в итоге не нашел ничего лучше, чем устраивать проверку на победу/поражение в функции кнопки.

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
+const $arenas = document.querySelector('.arenas')
+const $randomButton = document.querySelector('.button')
+
 const player1 = {
+    player: 1,
     name: 'SUB-ZERO',
-    hp: 80,
+    hp: 100,
     img: 'http://reactmarathon-api.herokuapp.com/assets/subzero.gif',
     weapon: ['Kunai','Sai'],
     attack: function() {
@@ -9,8 +13,9 @@ const player1 = {
 }
 
 const player2 = {
+    player: 2,
     name: 'KITANA',
-    hp: 50,
+    hp: 100,
     img: 'http://reactmarathon-api.herokuapp.com/assets/kitana.gif',
     weapon: ['Kunai','Sai'],
     attack: function() {
@@ -18,47 +23,82 @@ const player2 = {
     }
 }
 
-function createPlayer(playerName, playerInfo) {
+function createElement(tag, className) {
+    const $tag = document.createElement(tag)
 
-    const $player = document.createElement('div')
-    $player.classList.add(playerName)
+    if (className) {
+        $tag.classList.add(className)
+    }
 
-    const $arenas = document.querySelector('.arenas')
-    $arenas.appendChild($player)
+    return $tag
+}
+
+function createPlayer(playerInfo) {
+    const $player = createElement('div', 'player' + playerInfo.player)
+    const $progressbar = createElement('div','progressbar') 
+    const $life = createElement('div','life') 
+    const $name = createElement('div','name') 
+    const $p = createElement('p')
+    const $character = createElement('div','character')
+    const $img = createElement('img')
 
 
-    const $progressbar = document.createElement('div') 
-    $progressbar.classList.add('progressbar')
-
-    const $life = document.createElement('div') 
-    $life.classList.add('life')
     $life.style.width = (playerInfo.hp + '%')
-
-    const $name = document.createElement('div') 
-    $name.classList.add('name')
-
-    const $p = document.createElement('p')
     $p.innerText = playerInfo.name
+    $img.src = playerInfo.img
 
-    $name.appendChild($p)
 
     $progressbar.appendChild($life)
     $progressbar.appendChild($name)
 
-
-    const $character = document.createElement('div') 
-    $character.classList.add('character')
-
-    const $img = document.createElement('img')
-    $img.src = playerInfo.img
+    $name.appendChild($p)
 
     $character.appendChild($img)
-
 
     $player.appendChild($progressbar)
     $player.appendChild($character)
 
+    return $player
 }
 
-createPlayer('player1', player1)
-createPlayer('player2', player2)
+function changeHP(player) {
+    const $playerLife = document.querySelector('.player' + player.player + ' .life')
+    player.hp -= Math.ceil(Math.random()*20);
+
+    if (player.hp < 0) {
+        player.hp = 0
+    }
+    console.log(player.hp)
+
+    $playerLife.style.width = player.hp + '%'
+}
+
+function playerLose(player) {
+    const $loseTitle = createElement('div', 'loseTitle')
+
+    if (player) {
+        $loseTitle.innerText = player.name + ' win'
+    } else {
+        $loseTitle.innerText = 'draw'
+    }
+
+    $randomButton.disabled = true
+
+    return $loseTitle
+}
+
+$randomButton.addEventListener('click', function() {
+        changeHP(player1)
+        changeHP(player2)
+
+        if ((player1.hp > 0)&&(player2.hp == 0)) {
+            $arenas.appendChild(playerLose(player1))
+        } else if ((player1.hp == 0)&&(player2.hp > 0)) {
+            $arenas.appendChild(playerLose(player2))
+        } else if ((player1.hp == 0)&&(player2.hp == 0)) {
+            $arenas.appendChild(playerLose())
+        }
+})
+
+$arenas.appendChild(createPlayer(player1))
+$arenas.appendChild(createPlayer(player2))
